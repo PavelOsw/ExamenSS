320def1254152d4f488335b6cb8d784353365767
PavelOsw <pavel.olage@alumnos.udg.mx>
2022-05-30 16:55:16 -0500

    Examen Final

---
+++ database/migrations/2022_05_30_161529_create_materia_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materia', function (Blueprint $table) {
+            //$table->id('id_materia');
+            $table->integer('creditos')->require();
+            $table->string('nombre')->require();
+            $table->string('profesor')->require();
+            $table->string('turno')->require();
+            $table->boolean('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materia');
+    }
+};
